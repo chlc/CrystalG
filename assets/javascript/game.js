645508465fe30ec34949100b8969144faf656c45
Chelsea <TheGoldess@The-Goldess-2.local>
2017-09-25 00:49:56 -0500

    ediitng number values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
 
 $( document ).ready(function(){
-  var Random=Math.floor(Math.random()*101+19)
+  var Random=Math.floor(Math.random()*120+19)
   // Selects a random number to be shown at the start of the game
   // Number should be should be between 19 - 120
   //
   $('#randomNumber').text(Random);
   // Appending random number to the randomNumber id in the html doc
   //
-  var blue= Math.floor(Math.random()*11+1)
-  var blue2= Math.floor(Math.random()*11+1)
-  var white= Math.floor(Math.random()*11+1)
-  var purple= Math.floor(Math.random()*11+1)
+  var blue= Math.floor(Math.random()*12+1)
+  var blue2= Math.floor(Math.random()*12+1)
+  var white= Math.floor(Math.random()*12+1)
+  var purple= Math.floor(Math.random()*12+1)
   // Setting up random numbers for each jewel
   // Random number has to be between 1 - 12
   // 
@@ -22,13 +22,13 @@ $('#numberWins').text(wins);
 $('#numberLosses').text(losses);
 //Reset
 function reset(){
-      Random=Math.floor(Math.random()*101+19);
+      Random=Math.floor(Math.random()*120+19);
       console.log(Random)
       $('#randomNumber').text(Random);
-      blue= Math.floor(Math.random()*11+1);
-      blue2= Math.floor(Math.random()*11+1);
-      white= Math.floor(Math.random()*11+1);
-      purple= Math.floor(Math.random()*11+1);
+      blue= Math.floor(Math.random()*12+1);
+      blue2= Math.floor(Math.random()*12+1);
+      white= Math.floor(Math.random()*12+1);
+      purple= Math.floor(Math.random()*12+1);
       userTotal= 0;
       $('#finalTotal').text(userTotal);
       } 
